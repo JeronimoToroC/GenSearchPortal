50498c5904f12d1f99a84f73998083363f978fa9
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-11-25 21:40:05 -0500

    Screen de login es implementado

--- frontend/src/screens/login/login.component.ts
+++ frontend/src/screens/login/login.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonComponent } from '@common-components/Button/button.component';
 
 @Component({
-  selector: 'app-login',
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+    selector: 'app-login',
+    standalone: true,
+    imports: [FormsModule, ButtonComponent],
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+    email: string = '';
+    password: string = '';
 
+    onSubmit(): void {
+        // TODO: Implementar lógica de login
+        console.log('Login:', { email: this.email, password: this.password });
+    }
+
+    onSignUp(): void {
+        // TODO: Implementar redirección a registro
+        console.log('Redirigiendo a registro...');
+    }
 }
