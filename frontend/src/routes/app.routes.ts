dd2d9b7ad77e081a7b7a5ada513fc51fbe413f43
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-12-03 09:12:45 -0500

    Screen home es agregada

--- frontend/src/routes/app.routes.ts
+++ frontend/src/routes/app.routes.ts
@@ -1,14 +1,23 @@
 import { Routes } from '@angular/router'
 import { LoginComponent } from '@screens/login/login.component'
+import { HomeComponent } from '@screens/home/home.component'
 
 export const routes: Routes = [
     {
-        path: '**',
+        path: '',
         redirectTo: 'login',
         pathMatch: 'full',
     },
     {
         path: 'login',
         component: LoginComponent,
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+    },
+    {
+        path: '**',
+        redirectTo: 'login',
     },
 ]
