acf169a3b0e2419d27b9c7af4269bc8bf8767a27
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-12-03 08:53:55 -0500

    Estado global es movido de director

--- frontend/src/globalStates/user.globalstate.ts
+++ frontend/src/globalStates/user.globalstate.ts
@@ -1,23 +1,23 @@
 import { BehaviorSubject, Observable } from 'rxjs'
+import { UserModel } from '@models/user.model'
 import { Injectable } from '@angular/core'
-import { User } from '@models/user.model'
 
 @Injectable({
     providedIn: 'root',
 })
 export class UserGlobalState {
-    private readonly userSubject: BehaviorSubject<User | null> = new BehaviorSubject<User | null>(null)
+    private readonly userSubject: BehaviorSubject<UserModel | null> = new BehaviorSubject<UserModel | null>(null)
 
     // Observable público para que los componentes puedan suscribirse
-    public user$: Observable<User | null> = this.userSubject.asObservable()
+    public user$: Observable<UserModel | null> = this.userSubject.asObservable()
 
     // Getter para obtener el valor actual del usuario
-    get currentUser(): User | null {
+    get currentUser(): UserModel | null {
         return this.userSubject.getValue()
     }
 
     // Setter para actualizar el usuario
-    setUser(user: User | null): void {
+    setUser(user: UserModel | null): void {
         this.userSubject.next(user)
     }
 }
