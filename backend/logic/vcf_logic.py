8c861f149cc99044f58be95581dcef8e9a3e6b72
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-12-08 23:43:44 -0500

    Archivo es procesado correctamente

--- backend/logic/vcf_logic.py
+++ backend/logic/vcf_logic.py
@@ -1,5 +1,6 @@
 """VCF file handling logic."""
 
+import asyncio
 from fastapi import UploadFile
 from helpers.upload_vcf_file_helper import upload_file_vcf
 from helpers.insert_data_in_db_helper import procesar_archivo_vcf
@@ -10,6 +11,5 @@ class VcfLogic:
     @staticmethod
     async def upload_vcf_and_insert_data(file: UploadFile):
         """Procesar la subida de archivo VCF."""
-        # await upload_file_vcf(file)
-        # procesar_archivo_vcf(file.filename)
-        procesar_archivo_vcf("cabernetSauvignon.vcf")
+        await upload_file_vcf(file)
+        procesar_archivo_vcf(file.filename)
